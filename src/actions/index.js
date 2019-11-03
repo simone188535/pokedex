9eb4d310271addd1ca42c63ece603f63e66b9392
Simone Anthony <simoneanthony@Simones-MBP.attlocal.net>
2019-11-02 22:36:06 -0400

    plenty of edits

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,32 +1,42 @@
-import axios from 'axios';
+// import axios from 'axios';
+// import _ from 'lodash';
 
 export function viewAllAction() {
     return { type: 'TESTING' };
 }
 
-export function addItem({ sprites, species, height, weight }) {
+export function addItem({ name, sprites, types, stats, moves, height, weight }) {
     return {
         type: 'ADD_ITEM',
         payload: {
+            name,
             sprites,
-            species,
+            types,
+            stats,
+            moves,
             height,
             weight,
         }
     };
 }
 
-export const allListItems = () => {
-    return dispatch => {
-        axios.get(`https://pokeapi.co/api/v2/pokemon/?limit=100&offset=50`).then(res => {
-            // return res.data;
-            console.log(res.data);
-            // dispatch({type: 'FILTER_SEARCH', payload: res.data});
-        }).catch(e =>{
-            return console.log('could not fulfill asyc request', e);
-        });
-    }
-}
+// export const allListItems = () => {
+//     return dispatch => {
+//         axios.get(`https://pokeapi.co/api/v2/pokemon/?limit=50&offset=5`).then(res => {
+           
+//         // console.log(res.data.results)
+//          _.each((res.data.results),(value)=>{
+//                 return value.name;
+                
+//             });
+//             // dispatch({type: 'FILTER_SEARCH', payload: res.data});
+//         }).then(res =>{
+//             // console.log(res);
+//         }).catch(e =>{
+//             return console.log('could not fulfill asyc request', e);
+//         });
+//     }
+// }
 
 // export const allListItems = (searchValue) => {
 //     return dispatch => {
--- src/components/AllData.js
+++ src/components/AllData.js
@@ -1,53 +1,85 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 import { connect } from "react-redux";
+import _ from 'lodash';
 
 // import { viewAllAction } from '../actions/index';
-import { addItem,allListItems } from '../actions/index';
+import { addItem
+    // , allListItems 
+} from '../actions/index';
 
 
 class ImportData extends Component {
-    
-    // constructor(props){
-    //     super(props);
 
-    //     this.state = {
-    //         test: props.searchValue
-    //     }
-    // }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            allNames: []
+        }
+    }
     // state = {
     //     allData : []
     // }
+    grabAllNames = async () => {
+        try {
+            //this function grabs all names out of the api and stores them in the allNames array. 
+            //this info will later be fed into another api call. 
+            const res = await axios.get("https://pokeapi.co/api/v2/pokemon/?limit=50&offset=5");
+            await _.each((res.data.results), (value) => {
 
-    componentDidMount(){
+                 this.state.allNames.push(value.name);
+
+            });
+            // console.log(this.state.allNames);
+            await _.each((this.state.allNames), async (value) => {
+                const individualRes = await axios.get(`https://pokeapi.co/api/v2/pokemon/${value}`);
+                
+                console.log(individualRes.data);
+                const { name, sprites, types, stats, moves, height, weight } = individualRes.data;
+                const spritesFD = sprites.front_default;
+
+                // this.props.addItem({name, spritesFD, types, stats, moves, height, weight});
+            });
+
+        } catch (err) {
+            throw new Error('Unable to connect to API');
+        }
+        // console.log(this.state.allNames);
+
+
+    }
+    componentDidMount() {
         // axios.get("https://pokeapi.co/api/v2/pokemon/ditto/").then(res =>{
         //     const { sprites, species, height, weight } = res.data;
         //     // console.log(res.data);
         //     this.props.addItem({sprites, species, height, weight});
         // });
-
-        this.props.allListItems();
+        // axios.get("https://pokeapi.co/api/v2/pokemon/ditto/")
+        // this.props.allListItems();
         // this.props.viewAll();
         // console.log(this.props.searchValue);
+        this.grabAllNames();
+
     }
-    componentDidUpdate(){
+    componentDidUpdate() {
         console.log(this.props.searchValue);
         this.props.filterSearch(this.props.searchValue);
     }
-    
-    render(){
+
+    render() {
 
         return (<div>Importing data</div>);
     };
 }
 
-const mapDispatchToProps = (dispatch) =>({
+const mapDispatchToProps = (dispatch) => ({
     // viewAll: () => dispatch(viewAllAction())
     addItem: (payload) => dispatch(addItem(payload)),
-    allListItems: () => dispatch(allListItems())
+    // allListItems: () => dispatch(allListItems())
 });
 
 export default connect(
     null,
     mapDispatchToProps,
-  )(ImportData);
+)(ImportData);
