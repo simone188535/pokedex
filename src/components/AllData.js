196914b255b5d72f9ab6e3b955813ce63c431d7b
Simone Anthony <simoneanthony@Simones-MBP.attlocal.net>
2019-11-04 14:42:46 -0500

    redux insertion has been optimized

--- src/components/AllData.js
+++ src/components/AllData.js
@@ -26,12 +26,10 @@ class ImportData extends Component {
         try {
             //this function grabs all names out of the api and stores them in the allNames array. 
             //this info will later be fed into another api call. 
-            const res = await axios.get("https://pokeapi.co/api/v2/pokemon/?limit=4");
-            const allNames = []
-            _.each((res.data.results), (value) => {
-
-                allNames.push(value.name);
-
+            const res = await axios.get("https://pokeapi.co/api/v2/pokemon/?limit=150");
+            //const allNames = [];
+            const allNames = _.map((res.data.results), (value) => {
+                return value.name
             });
             this.useNamesToInputDataIntoRedux(allNames);
 
@@ -45,75 +43,54 @@ class ImportData extends Component {
             
             const promisesArray = _.map(allNames, async (value) => {
                 const res= await axios.get(`https://pokeapi.co/api/v2/pokemon/${value}`);
-                return res.data;
+                return res;
             });
             // console.log(promisesArray);
             const valuesArray = await axios.all(promisesArray);
-            console.log(valuesArray); // Should have resolved values
-            
-            //    let axiosRes = axios.all(promisesArray);
-            //    console.log(axiosRes);
-            // await Promise.all([ promisesArray]).then((res) => {
-            //     console.log(res);
-            //   });
+            // console.log(valuesArray); // Should have resolved values
            
-              
-            // let test = [1,2,3];
-            // console.log(test);
-            // _.each((test), (value) => {
-            //     console.log(value);
-            // });
+            _.each((valuesArray), (res) =>{
+                
+                const { name, sprites, types, stats, moves, height, weight } = res.data;
+                // console.log(name);
+
+                    //extracts front default sprite 
+                const sprite = sprites.front_default;
+
+                //extract types
+                let allTypes = _.map((types), (value) => {
+                    //iterates through types array and puts in to allTypes array
+                    return value.type.name;
+                });
+                // console.log(allTypes);
+
+                //extract stats
+                let allStats = _.map((stats), (value) => {
+                    let statsData = { name: value.stat.name, base_stat: value.base_stat };
+                    //iterates through stat array and puts in to allStats array (there are 6 values for each)
+                    return statsData;
+                    // console.log(statsData);
+                });
+                // console.log(allStats);
+                
+                //extracts the first 4 moves for each pokemon. 
+                let allMoves = [];
+                _.each((moves), (value, key) => {
+                    //iterates through moves array and puts in to allMoves array
+                    allMoves.push(value.move.name);
+
+                    //this stops the array after first 4 moves
+                    if (key === 3) {
+                        return false;
+                    }
+                });
+                // console.log(allMoves);
+
+
+
+               this.props.addItem({ name, sprite, allTypes, allStats, allMoves, height, weight });
+            });
             
-            
-            
-            // _.map((allNames), async (value) => {
-            //     const individualRes = await axios.get(`https://pokeapi.co/api/v2/pokemon/${value}`);
-
-            //     // return console.log('test1');
-            //     // this shows all the data from individual responses. Use this to show all data
-            //     // console.log(individualRes.data);
-
-            //     const { name, sprites, types, stats, moves, height, weight } = individualRes.data;
-
-            //     //extracts front default sprite 
-            //     const sprite = sprites.front_default;
-
-            //     //extract types
-            //     let allTypes = [];
-            //     _.each((types), (value) => {
-            //         //iterates through types array and puts in to allTypes array
-            //         allTypes.push(value.type.name);
-            //     });
-            //     // console.log(allTypes);
-
-            //     //extract stats
-            //     let allStats = [];
-            //     _.each((stats), (value) => {
-            //         let statsData = { name: value.stat.name, base_stat: value.base_stat };
-            //         //iterates through stat array and puts in to allStats array (there are 6 values for each)
-            //         allStats.push(statsData);
-            //         // console.log(statsData);
-            //     });
-            //     // console.log(allStats);
-
-            //     //extracts the first 4 moves for each pokemon. 
-            //     let allMoves = [];
-            //     _.each((moves), (value, key) => {
-            //         //iterates through moves array and puts in to allMoves array
-            //         allMoves.push(value.move.name);
-
-            //         //this stops the array after first 4 moves
-            //         if (key === 3) {
-            //             return false;
-            //         }
-            //     });
-            //     // console.log(allMoves);
-
-
-            //     console.log(this.props.addItem({ name, sprite, allTypes, allStats, allMoves, height, weight }));
-
-            // });
-
             // //render list after data is loaded
             // this.renderList();
             
